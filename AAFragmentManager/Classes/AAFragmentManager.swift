e2bc223b87bc53c6947fc0141bf2281047e011a6
Muhammad Ahsan <muhammad.ahsan@allshores-macbook-pro.local>
2017-02-03 19:26:56 +0500

    Added previous and next fragment replacement.

--- AAFragmentManager/Classes/AAFragmentManager.swift
+++ AAFragmentManager/Classes/AAFragmentManager.swift
@@ -8,34 +8,48 @@
 
 import UIKit
 
-
 open class AAFragmentManager: UIView {
     
+    /// Transition object for animation between fragment
+    open var transition: CATransition = CATransition()
+    
+    /// Allow same fragment to replace
+    open var allowSameFragment: Bool = false
+    
+    /// Selected Fragment Index
+    var selectedIndex: Int = -1
+
+    /// Fragments
     var views: [UIViewController]?
     
-    let transition: CATransition = CATransition()
     
-    var currentFragmentIndex: Int = -1
-    
+    /// Init function
+    ///
+    /// - Parameter frame: frame
     override init (frame : CGRect) {
         super.init(frame : frame)
-        
-    }
-    
-    open func setupFragments(_ childViews: [UIViewController], defaultIndex: Int = -1) {
-        self.views = childViews
-        initTranstion()
-        if viewExists(defaultIndex) {
-            replaceFragment(index: defaultIndex, shouldAnimate: false)
-        }
-
     }
     
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
+    
+    /// Init fragments with child view contrllers
+    ///
+    /// - Parameters:
+    ///   - childViews: fragments
+    ///   - defaultIndex: default fragment index
+    open func setupFragments(_ childViews: [UIViewController], defaultIndex: Int = -1) {
+        self.views = childViews
+        setupTranstion()
+        if viewExists(defaultIndex) {
+            replaceFragment(withIndex: defaultIndex, shouldAnimate: false)
+        }
 
-    func initTranstion() {
+    }
+    
+    /// Setup Animation
+    func setupTranstion() {
         
         transition.duration = 0.3
         transition.type = kCATransitionReveal
@@ -44,49 +58,59 @@ open class AAFragmentManager: UIView {
         
     }
     
-    func viewExists(_ index: Int) -> Bool {
-        return views?.indices.contains(index) ?? false
-    }
-    
-    open func getFragment(_ index: Int) -> UIViewController? {
-        if viewExists(index) {
-            return views![index]
-        }
-        return nil
-    }
-    
-    open func replaceFragment(index: Int, shouldAnimate animate: Bool = true, shouldFit fit: Bool = true, allowSameFragment sameFragment: Bool = false) {
+
+    /// Replaces fragment by index in current view
+    ///
+    /// - Parameters:
+    ///   - index: fragment index
+    ///   - animate: should animate
+    ///   - fit: should fit in bounds
+    open func replaceFragment(withIndex index: Int, shouldAnimate animate: Bool = true, shouldFit fit: Bool = true) {
         
-        guard viewExists(index) else {
+        guard
+            viewExists(index),
+            let view = views?[index].view,
+            !(!allowSameFragment && selectedIndex == index) else {
             return
         }
         
-        if let view = views?[index].view {
-            
-            guard !(!sameFragment && currentFragmentIndex == index) else {
-                return
-            }
-            
-            for view in self.subviews {
-                view.removeFromSuperview()
-            }
-            
-            if animate {
-                transition.subtype = currentFragmentIndex > index ? kCATransitionFromLeft : kCATransitionFromRight
-                self.layer.add(transition, forKey: kCATransition)
-            }
-            else {
-                self.layer.removeAnimation(forKey: kCATransition)
-            }
-            currentFragmentIndex = index
-
-            view.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: view.frame.height)
-
-            let heightView = fit ? view : self
-            view.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: heightView.frame.height)
-            self.addSubview(view)
-            
+        removeSubviews()
+        
+        
+        if animate {
+            transition.subtype = selectedIndex > index ? kCATransitionFromLeft : kCATransitionFromRight
+            self.layer.add(transition, forKey: kCATransition)
         }
+        else {
+            self.layer.removeAnimation(forKey: kCATransition)
+        }
+        
+        selectedIndex = index
+
+        let heightView = fit ? self : view
+        view.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: heightView.frame.height)
+        self.addSubview(view)
+
+    }
+    
+    /// Replaces with next fragment if exists
+    ///
+    /// - Parameters:
+    ///   - animate: should animate
+    ///   - fit: should fit
+    open func nextFragment(_ animate: Bool = true, shouldFit fit: Bool = true) {
+        let nextIndex = selectedIndex + 1
+        replaceFragment(withIndex: nextIndex, shouldAnimate: animate, shouldFit: fit)
+    }
+    
+    /// Replaces with previous fragment if exists
+    ///
+    /// - Parameters:
+    ///   - animate: should animate
+    ///   - fit: should fit
+    open func previousFragment(_ animate: Bool = true, shouldFit fit: Bool = true) {
+        let nextIndex = selectedIndex - 1
+        replaceFragment(withIndex: nextIndex, shouldAnimate: animate, shouldFit: fit)
     }
 }
 
--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 # Table of Contents
 
 - [AAFragmentManager](#section-id-5)
-  - [Description](#section-id-12)
-  - [Demonstration](#section-id-17)
-  - [Requirements](#section-id-27)
+- [Description](#section-id-12)
+- [Demonstration](#section-id-17)
+- [Requirements](#section-id-27)
 - [Installation](#section-id-33)
-  - [CocoaPods](#section-id-39)
-  - [Carthage](#section-id-65)
-  - [Manual Installation](#section-id-83)
+- [CocoaPods](#section-id-39)
+- [Carthage](#section-id-65)
+- [Manual Installation](#section-id-83)
 - [Getting Started](#section-id-87)
-  - [Create your fragment manager](#section-id-90)
-  - [Setup your child views](#section-id-101)
-  - [Get any child views controller](#section-id-112)
-  - [Switching between child views](#section-id-123)
+- [Create your fragment manager](#section-id-90)
+- [Setup your child views](#section-id-101)
+- [Get any child views controller](#section-id-112)
+- [Switching between child views](#section-id-123)
 - [Contributions & License](#section-id-142)
 
 
@@ -140,6 +140,7 @@ Create a `UIView` and set the class as `AAFragmentManager`. Make an `IBOutlet` o
 Now create child views (fragments) using single line
 
 ```ruby
+childView.allowSameFragment = true
 childView.setupFragments(arrayVC, defaultIndex: 0)
 ```
 
@@ -166,15 +167,20 @@ demoFragment.delegate = self // sets for some callback
 You can switch child view controllers using the single line
 
 ```ruby
+// replace fragment by index
 childView.replaceFragment(index: 1)
+// replace with next fragment
+childView.previousFragment()
+// replace with previous fragment
+childView.nextFragment()
 ```
 
-> Note that replaceFragment has optional parameters `shouldAnimate` , `shouldFit`,  `allowSameFragment`.
+> Note that replaceFragment has optional parameters `shouldAnimate` , `shouldFit`.
 
 Here's the method signature
 
 ```ruby
-open func replaceFragment(index: Int, shouldAnimate animate: Bool = true, shouldFit fit: Bool = true, allowSameFragment sameFragment: Bool = false)
+open func replaceFragment(index: Int, shouldAnimate animate: Bool = true, shouldFit fit: Bool = true)
 ```
 
 > By default, `shouldFit` is true such that it will make fragment of same height as its parent view and vice versa. 
